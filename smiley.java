8c71093bbef7fd0ea419a007f076890cd3c7e53f
Yash Jain <60340147+yashjain072@users.noreply.github.com>
2021-02-20 10:08:10 +0530

    Add files via upload

---
+++ smiley.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class smiley extends Frame {
+    draw1() {
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.drawRect(40, 40, 200, 200);
+        g.drawOval(90, 70, 80, 80);
+        g.drawOval(110, 95, 5, 5);
+        g.drawOval(145, 95, 5, 5);
+        g.drawLine(130, 95, 130, 115);
+        g.drawArc(113, 115, 35, 20, 0, -180);
+    }
+
+
+    public static void main(String[] args) {
+        smiley d = new smiley();
+        d.setSize(400, 400);
+        d.setTitle("smile");
+        d.setVisible(true);
+
+    }
+}
