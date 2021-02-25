361a89cdd8ef1489a18839657bf86b0c90724491
Yash Jain <60340147+yashjain072@users.noreply.github.com>
2021-02-25 11:20:50 +0530

    Creating Frame with Title

---
+++ Frame3.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+public class Frame3
+{
+	public static void main(String args[])
+	{
+		Frame f=new Frame();
+		f.setTitle("Frame With Title");
+		f.setSize(300,300);
+		f.setVisible(true);
+	}
+}
